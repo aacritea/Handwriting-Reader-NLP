08a14c366bfba85c63853593738e2c0f3bda857d
Aakriti Jain <163650998+aacritea@users.noreply.github.com>
2024-08-27 22:09:00 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,3 +1,82 @@
 from emnist import extract_training_samples
 print("Imported the EMNIST libraries we need!")
 
+# Grab the data from the OpenML website
+# X will be our images and y will be the labels
+X, y = extract_training_samples('letters')
+
+# Make sure that every pixel in all of the images is a value between 0 and 1
+X = X / 255.
+
+# Use the first 60000 instances as training and the next 10000 as testing
+X_train, X_test = X[:60000], X[60000:70000]
+y_train, y_test = y[:60000], y[60000:70000]
+
+# There is one other thing we need to do, we need to
+# record the number of samples in each dataset and the number of pixels in each image
+X_train = X_train.reshape(60000,784)
+X_test = X_test.reshape(10000,784)
+
+print("Extracted our samples and divided our training and testing data sets")
+
+import matplotlib.pyplot as plt
+
+img_index = 14000 # <<<<<  You can update this value to look at other images
+img = X_train[img_index]
+print("Image Label: " + str(chr(y_train[img_index]+96)))
+plt.imshow(img.reshape((28,28)))
+
+# These two lines import the ML libraries we need
+from sklearn.datasets import fetch_openml
+from sklearn.neural_network import MLPClassifier
+
+# This creates our first MLP with 1 hidden layer with 50 neurons and sets it to run through the data 20 times
+mlp1 = MLPClassifier(hidden_layer_sizes=(50,), max_iter=20, alpha=1e-4,
+                    solver='sgd', verbose=10, tol=1e-4, random_state=1,
+                    learning_rate_init=.1)
+
+print("Created our first MLP network")
+
+mlp1.fit(X_train, y_train)
+print("Training set score: %f" % mlp1.score(X_train, y_train))
+print("Test set score: %f" % mlp1.score(X_test, y_test))
+
+# First let's initialize a list with all the predicted values from the training set
+y_pred = mlp1.predict(X_test)
+
+# Now let's visualize the errors between the predictions and the actual labels using a confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+plt.matshow(cm)
+
+# You can change this to any letters that you think the neural network may have confused...
+predicted_letter = 'l'
+actual_letter = 'i'
+
+
+# This code counts all mistakes for the letters above
+mistake_list = []
+for i in range(len(y_test)):
+  if (y_test[i] == (ord(actual_letter) - 96) and y_pred[i] == (ord(predicted_letter) - 96)):
+    mistake_list.append(i)
+print("There were " + str(len(mistake_list)) + " times that the letter " + actual_letter + " was predicted to be the letter " + predicted_letter + ".")
+
+# Once we know how many mistakes were made, we can change this to see an image of a particular one
+mistake_to_show = 4 # <<< e.g., change this to 3 if you want to see the 4th mistake
+
+# This code checks that the number mistake you asked for can be shown and if so, displays an image of it
+if (len(mistake_list)> mistake_to_show):
+  img = X_test[mistake_list[mistake_to_show]]
+  plt.imshow(img.reshape((28,28)))
+else:
+  print("Couldn't show mistake number " + str(mistake_to_show + 1) + " because there were only " + str(len(mistake_list)) + " mistakes to show!")
+
+# Change some of the values in the below statement and re-run to see how they 
+# affect performance!
+mlp2 = MLPClassifier(hidden_layer_sizes=(100,100,100,100,100,), max_iter=50, alpha=1e-4,
+                    solver='sgd', verbose=10, tol=1e-4, random_state=1,
+                    learning_rate_init=.1)
+mlp2.fit(X_train, y_train)
+print("Training set score: %f" % mlp2.score(X_train, y_train))
+print("Test set score: %f" % mlp2.score(X_test, y_test))
+
