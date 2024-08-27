1d8adba895797d2045a17eea2313b328308efe3a
Aakriti Jain <163650998+aacritea@users.noreply.github.com>
2024-08-27 23:09:09 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -80,3 +80,111 @@ mlp2.fit(X_train, y_train)
 print("Training set score: %f" % mlp2.score(X_train, y_train))
 print("Test set score: %f" % mlp2.score(X_test, y_test))
 
+# Puts all the data in the "files" variable
+import os
+path, dirs, files = next(os.walk("/content/lab1-neural-networks/letters_mod/"))
+files.sort()
+
+# These libraries let us import the letters, resize them, and print them out
+import cv2
+import matplotlib.pyplot as plt
+
+# This code processes all the scanned images and adds them to the handwritten_story
+handwritten_story = []
+for i in range(len(files)):
+  img = cv2.imread("/content/lab1-neural-networks/letters_mod/"+files[i],cv2.IMREAD_GRAYSCALE)
+  handwritten_story.append(img)
+
+print("Imported the scanned images.")
+
+plt.imshow(handwritten_story[4])  #<--- Change this index to see different letters
+
+# These are libraries we need to do some math on the image
+# to be able to give it to the MLP in the right format and to resize it to 28x28 pixels
+import numpy
+import cv2
+
+typed_story = ""
+for letter in handwritten_story:
+    letter = cv2.resize(letter, (28,28), interpolation = cv2.INTER_CUBIC)
+    single_item_array = (numpy.array(letter)).reshape(1,784)
+    prediction = mlp2.predict(single_item_array)
+    typed_story = typed_story + str(chr(prediction[0]+96))
+    
+print("Conversion to typed story complete!")
+print(typed_story)
+
+# This is a library we need to do some math on the image to be able to give it to the MLP in the right format
+import numpy
+
+typed_story = ""
+for letter in handwritten_story:
+  letter = cv2.resize(letter, (28,28), interpolation = cv2.INTER_CUBIC)
+    
+  #this bit of code checks to see if the image is just a blank space by looking at the color of all the pixels summed
+  total_pixel_value = 0
+  for j in range(28):
+    for k in range(28):
+      total_pixel_value += letter[j,k]
+  if total_pixel_value < 20:
+    typed_story = typed_story + " "
+  else:         #if it NOT a blank, it actually runs the prediction algorithm on it
+    single_item_array = (numpy.array(letter)).reshape(1,784)
+    prediction = mlp2.predict(single_item_array)
+    typed_story = typed_story + str(chr(prediction[0]+96))
+    
+print("Conversion to typed story complete!")
+print(typed_story)
+
+# These steps process the scanned images to be in the same format and have the same properties as the EMNIST images
+# They are described by the EMNIST authors in detail here: https://arxiv.org/abs/1702.05373v1
+processed_story = []
+
+for img in handwritten_story:
+  #step 1: Apply Gaussian blur filter
+  img = cv2.GaussianBlur(img, (7,7), 0)
+  
+  #steps 2 and 3: Extract the Region of Interest in the image and center in square
+  points = cv2.findNonZero(img)
+  x, y, w, h = cv2.boundingRect(points)
+  if (w > 0 and h > 0):
+    if w > h:
+      y = y - (w-h)//2
+      img = img[y:y+w, x:x+w]
+    else:
+      x = x - (h-w)//2
+      img = img[y:y+h, x:x+h]
+     
+  #step 4: Resize and resample to be 28 x 28 pixels
+  img = cv2.resize(img, (28,28), interpolation = cv2.INTER_CUBIC)
+  
+  #step 5: Normalize pixels and reshape before adding to the new story array
+  img = img/255
+  img = img.reshape((28,28))
+  processed_story.append(img)
+
+print("Processed the scanned images.")
+
+import matplotlib.pyplot as plt
+plt.imshow(processed_story[4]) #<<< change this index if you want to see a different letter from the story
+
+# This is a library we need to do some math on the image to be able to give it to the MLP in the right format
+import numpy
+
+typed_story = ""
+for letter in processed_story:
+  #this bit of code checks to see if the image is just a blank space by looking at the color of all the pixels summed
+  total_pixel_value = 0
+  for j in range(28):
+    for k in range(28):
+      total_pixel_value += letter[j,k]
+  if total_pixel_value < 20:
+    typed_story = typed_story + " "
+  else:         #if it NOT a blank, it actually runs the prediction algorithm on it
+    single_item_array = (numpy.array(letter)).reshape(1,784)
+    prediction = mlp2.predict(single_item_array)
+    typed_story = typed_story + str(chr(prediction[0]+96))
+    
+print("Conversion to typed story complete!")
+print(typed_story)
+
